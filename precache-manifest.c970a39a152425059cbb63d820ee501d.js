63242cbb45303c2bf2793f9f118a1041663571b3
Wiley Buchanan <swagg187@Wileys-MacBook-Pro.local>
2019-04-05 15:57:33 -0700

    Updates

---
+++ precache-manifest.c970a39a152425059cbb63d820ee501d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8d6485433e0e7bb5908d67b5cf3a73a0",
+    "url": "/boo/static/media/boo.8d648543.jpg"
+  },
+  {
+    "revision": "b0712844539db8fd8d63",
+    "url": "/boo/static/js/runtime~main.ea351700.js"
+  },
+  {
+    "revision": "1b04426badb22089437c",
+    "url": "/boo/static/js/main.8db6ed56.chunk.js"
+  },
+  {
+    "revision": "a4bfaf8c4bccdd0a2cf9",
+    "url": "/boo/static/js/2.0771dcc1.chunk.js"
+  },
+  {
+    "revision": "1b04426badb22089437c",
+    "url": "/boo/static/css/main.beef79a1.chunk.css"
+  },
+  {
+    "revision": "f9cd4f1e6ebb10e71ee5f1de65cf7157",
+    "url": "/boo/index.html"
+  }
+];
